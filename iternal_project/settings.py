18bc0b20e86a544cb94723ceef4bdb6bfb88d751
neha.ambre <neha.ambre@cumminscollege.in>
2022-08-06 21:49:40 +0530

    6-8-22

--- iternal_project/settings.py
+++ iternal_project/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-d*c!dwvqi=hp732cphk1zwh@u0fk3+g!k17s2tmajcdbbj1mq='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1','internship-project-2022.herokuapp.com']
 
